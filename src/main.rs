c0a0abb502906355e5709c6fcb6cc3491e1d9bb4
Kevin Butler <haqkrs@gmail.com>
2015-03-09 20:28:38 +0000

    Fixup main binary

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
+#![feature(path)]
+#![cfg(not(test))]
+
 extern crate hack_interpreter;
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate docopt;
 
-#[cfg(not(test))]
+use std::path::AsPath;
+use hack_interpreter::{Rom, Cpu, runner};
+use docopt::Docopt;
+
+static USAGE: &'static str = "
+Usage: hack-interpreter [-r] <input>
+
+Options:
+    -r, --runner  Run a .tst file
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_input: String,
+    flag_runner: bool,
+}
+
 fn main() {
-    // use hack_interpreter::{Rom, Cpu};
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.decode())
+                                       .unwrap_or_else(|e| e.exit());
+    let input = &args.arg_input;
 
-    // let program = Rom::from_file("test.hack");
-    // let cpu = Cpu::new(program);
+    if args.flag_runner {
+        println!("Running test file: '{}'", input);
+        match runner(input.as_path()) {
+            Ok(()) => println!("Test completed successfully"),
+            Err(e) => panic!("{}", e),
+        }
+    } else {
+        println!("Running program file: '{}'", input);
 
-    // for _ in ..60 {
-    //     cpu.step()
-    // }
+        let program = Rom::from_file(input).unwrap();
+        let mut cpu = Cpu::new(program);
+
+        loop {
+            cpu.step()
+
+            //TODO: Render
+        }
+    }
 }
