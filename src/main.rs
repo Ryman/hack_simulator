b6218874884d88b75575a195bb4aeca7037ecac8
Kevin Butler <haqkrs@gmail.com>
2015-03-10 02:32:11 +0000

    Implement basic simulator

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-#![feature(path, io)]
+#![feature(path)]
 #![cfg(not(test))]
 
 extern crate hack_interpreter;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
-extern crate drawille;
+extern crate piston;
+extern crate image;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate sdl2_window;
 
-use std::sync::mpsc::{channel, TryRecvError};
-use std::thread;
-use std::io::{stdin, Read};
 use std::path::AsPath;
-use hack_interpreter::{Rom, Cpu, runner};
+use hack_interpreter::runner;
 use docopt::Docopt;
-use drawille::braille::Canvas;
+use simulator::run_simulator;
 
+mod simulator;
 
 static USAGE: &'static str = "
 Usage: hack-interpreter [-r] <input>
@@ -40,65 +42,6 @@ fn main() {
             Err(e) => panic!("{}", e),
         }
     } else {
-        render_simulation(input);
-    }
-}
-
-fn render_simulation(input: &String) {
-    println!("Running program file: '{}'", input);
-
-    let program = Rom::from_file(input).unwrap();
-    let mut cpu = Cpu::new(program);
-
-    let (tx, rx) = channel();
-
-    // Place stdin on other channel
-    thread::spawn(move|| {
-        let mut input = stdin();
-
-        loop {
-            // Read the keyboard
-            let c = &mut [0; 2];
-            let bytes_read = input.read(c).unwrap();
-
-            tx.send((c[0] as u16) << 8 | c[1] as u16).unwrap();
-        }
-    });
-
-    // Memory starts at 16384
-    // The screen has resolution 512 x 256
-    // Each bit of the screen represents
-    const WIDTH: usize = 512;
-    const HEIGHT: usize = 256;
-    const VIDEO_MEM_BYTES: usize = (WIDTH * HEIGHT) / 16;
-    let mut canvas = Canvas::new(WIDTH, HEIGHT);
-
-    for steps in (0..) {
-        cpu.step();
-
-        if steps % 10000000 == 0 {
-            println!("{}", canvas.frame());
-
-            cpu.ram[24576] = match rx.try_recv() {
-                Ok(key) => key,
-                Err(TryRecvError::Empty) => 0,
-                Err(TryRecvError::Disconnected) => panic!("Stdio thread died")
-            };
-
-            let video_mem = &cpu.ram[16384..16384 + VIDEO_MEM_BYTES];
-
-            for (idx, word) in video_mem.iter().enumerate() {
-                // For each word of memory, draw 16 pixels
-                let idx = idx * 16;
-                for (bit, i) in (idx..idx+16).enumerate() {
-                    let (x, y) = (i % WIDTH, i / WIDTH);
-                    if word & (1 << bit) != 0 {
-                        canvas.set(x, y);
-                    } else {
-                        canvas.unset(x, y);
-                    }
-                }
-            }
-        }
+        run_simulator(input);
     }
 }
---
+++ src/simulator.rs
@@ -0,0 +1,95 @@
+use hack_interpreter::{Rom, Cpu};
+
+use piston;
+use piston::quack::Set;
+use piston::window::WindowSettings;
+use piston::input::Button;
+use piston::input::keyboard::Key;
+use piston::event::{PressEvent, ReleaseEvent, IdleEvent, RenderEvent, UpdateEvent};
+use piston::event::{MaxFps, Ups};
+use graphics;
+use opengl_graphics::{GlGraphics, Texture, OpenGL};
+use image::{Rgba, ImageBuffer, GenericImage};
+use sdl2_window::Sdl2Window as Window;
+
+const WIDTH: usize = 512;
+const HEIGHT: usize = 256;
+const SCALE: usize = 2;
+const SCREEN_ADDR: usize = 16384;
+const SCREEN_MEMORY_LEN: usize = WIDTH * HEIGHT / 16;
+const KEYBOARD_ADDR: usize = SCREEN_ADDR + SCREEN_MEMORY_LEN;
+
+// TODO: MATH - Decide the MHz of the Cpu, partition it between frames
+const CYCLES_PER_IDLE: usize = 20000;
+const MAX_FPS: u64 = 30;
+const UPDATES_PER_SEC: u64 = 30;
+
+pub fn run_simulator(input: &str) {
+    let program = Rom::from_file(&input).unwrap();
+    let ref mut cpu = Cpu::new(program);
+    println!("Running program file: '{}'", input);
+
+    let gl_version = OpenGL::_3_2;
+    let window = Window::new(
+        gl_version,
+        WindowSettings {
+            title: format!("hack-interpreter: {}", input),
+            size: [(WIDTH * SCALE) as u32,
+                   (HEIGHT * SCALE) as u32],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 0,
+        }
+    );
+
+    let ref mut image = ImageBuffer::new((WIDTH * SCALE) as u32, (HEIGHT * SCALE) as u32);
+    let mut texture = Texture::from_image(&image);
+    let ref mut gl = GlGraphics::new(gl_version);
+
+    for e in piston::events(window)
+                    .set(Ups(UPDATES_PER_SEC))
+                    .set(MaxFps(MAX_FPS)) {
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            cpu.ram[KEYBOARD_ADDR] = key as u16;
+        }
+
+        if let Some(Button::Keyboard(_)) = e.release_args() {
+            cpu.ram[KEYBOARD_ADDR] = 0;
+        }
+
+        if let Some(args) = e.render_args() {
+            gl.draw(
+                [0, 0, args.width as i32, args.height as i32],
+                |c, g| graphics::image(&texture, &c, g)
+            );
+        }
+
+        e.update(|_| {
+            render_screen(image, &cpu);
+            texture.update(image);
+        });
+
+        e.idle(|_| for _ in (0..CYCLES_PER_IDLE) { cpu.step() });
+    }
+}
+
+fn render_screen(image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, cpu: &Cpu) {
+    let screen = &cpu.ram[SCREEN_ADDR..SCREEN_ADDR + SCREEN_MEMORY_LEN];
+
+    for (idx, word) in screen.iter().enumerate() {
+        // For each word of memory, draw 16 pixels
+        let idx = idx * 16;
+        for (bit, i) in (idx..idx + 16).enumerate() {
+            let (x, y) = ((i % WIDTH) * SCALE, (i / WIDTH) * SCALE);
+            let color = if word & (1 << bit) != 0 { 0 } else { 255 };
+
+            for i in (0..SCALE) {
+                for j in (0..SCALE) {
+                    image.put_pixel((x + i) as u32,
+                                    (y + j) as u32,
+                                    Rgba([color, color, color, 255]));
+                }
+            }
+        }
+    }
+}
