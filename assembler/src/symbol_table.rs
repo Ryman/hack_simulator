8c50a225bb17eb033b06239efc8bed82c35c833b
Kevin Butler <haqkrs@gmail.com>
2015-03-16 20:31:52 +0000

    Implement assembler::SymbolTable

--- assembler/src/symbol_table.rs
+++ assembler/src/symbol_table.rs
@@ -1,8 +1,21 @@
-pub struct SymbolTable;
+use std::collections::HashMap;
 
-impl SymbolTable {
-    pub fn new() -> SymbolTable { unimplemented!() }
-    pub fn contains(&self, key: &str) -> bool { unimplemented!() }
-    pub fn get_address(&self, key: &str) -> u16 { unimplemented!() }
-    pub fn add_entry(&mut self, key: &str, value: u16) { unimplemented!() }
+pub struct SymbolTable<'a>(HashMap<&'a str, u16>);
+
+impl<'a> SymbolTable<'a> {
+    pub fn new() -> SymbolTable<'a> {
+        SymbolTable(HashMap::new())
+    }
+
+    pub fn contains(&self, key: &str) -> bool {
+        self.0.contains_key(key)
+    }
+
+    pub fn get_address(&self, key: &str) -> u16 {
+        self.0[key]
+    }
+
+    pub fn add_entry(&mut self, key: &'a str, value: u16) {
+        self.0.insert(key, value);
+    }
 }
