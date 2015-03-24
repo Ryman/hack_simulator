6ccc3b8a17b706043fcb89dd3d10d49f1383547a
Kevin Butler <haqkrs@gmail.com>
2015-03-24 17:15:13 +0000

    Fix for changes in piston graphics

--- src/simulator.rs
+++ src/simulator.rs
@@ -68,7 +68,7 @@ pub fn run_simulator(input: &str) {
         if let Some(args) = e.render_args() {
             gl.draw(
                 [0, 0, args.width as i32, args.height as i32],
-                |c, g| graphics::image(&texture, &c, g)
+                |c, g| graphics::image(&texture, c.transform, g)
             );
         }
 
